dd4b08f3173001290e5dfb5f69302ebbc6e8e635
William Stach <ashnwill49@gmail.com>
2020-12-05 03:44:05 -0700

    initial andromeda bootloader exists

--- OS/Loader/loader.h
+++ OS/Loader/loader.h
@@ -2,4 +2,47 @@ extern ArgsInit { argp -- }
 extern ArgsValue { arg -- out }
 extern ArgsCheck { arg -- present }
 
+extern LoaderPanic { ... fmt -- }
+
+extern AFSInit { -- }
+extern AFSExit { -- }
+extern AFSMount { dev -- ok }
+extern AFSRead { buf len seek ino -- bytes }
+extern AFSPath { path -- ino }
+extern AFSFreeINode { ino -- }
+
+extern MemInit { -- }
+
+extern LOFFLoad { path -- sz entry }
+
 externptr LoaderTotalRAM
+externptr AFSDevice
+
+struct AFSSuperblock
+	4 Version
+	4 Magic
+	4 Dirty
+	4 NumReservedBlocks
+	4 FATStart
+	4 FATSize
+	4 IStart
+	4 ICount
+	4 DataStart
+	4 DataSize
+endstruct
+
+struct AFSDirEnt
+	4 INum
+	60 Name
+endstruct
+
+struct AFSINode
+	4 Type
+	4 Permissions
+	4 UID
+	4 Reserved1
+	4 Timestamp
+	4 StartBlock
+	4 Reserved2
+	4 Size
+endstruct

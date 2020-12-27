6da495dd22c7066df5a2fb9c6574a252171ee21e
Will <ashnwill49@gmail.com>
2020-12-27 05:31:08 -0700

    implement buddy allocator
    hopefully works, tested fine but who knows

--- OS/Kernel/include/KeMem.h
+++ OS/Kernel/include/KeMem.h
@@ -1,3 +1,15 @@
+struct KeMemBlock
+	4 Magic
+	4 Next
+	4 Prev
+	4 BucketIndex
+	4 Tag
+endstruct
+
+const KEMEMBLOCKMAGIC 0xC4FED33D
+
+const KEMEMBLOCKFREE 'FREE'
+
 struct PFDBE
 	4 R1
 	4 PFN
@@ -34,3 +46,12 @@ endstruct
 externptr KeMemPageFrameDB
 
 externptr KeMemPageFreeListHead
+
+externptr KeMemBuckets
+
+externptr KeMemHeapSize
+
+externptr KeMemHeap
+
+const BUCKETSPO2START 5 // start at 2^5=32 bytes
+const BUCKETS 23 // 2^(23+5=28)=256MB heap max

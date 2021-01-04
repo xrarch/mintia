2c8a936949eac1d7441f2352147e75e60c32e2e5
hyenasky <ashnwill49@gmail.com>
2021-01-04 01:59:36 -0700

    remove superfluous struct

--- OS/Kernel/include/KeTimer.h
+++ OS/Kernel/include/KeTimer.h
@@ -17,13 +17,6 @@ struct KeTimerH
 	4 ExpiryTimeMs
 endstruct
 
-struct KeTimerD
-	4 Next
-	4 Prev
-	KeTime_SIZEOF ExpiryTime
-	4 DPCNext
-endstruct
-
 extern KeTimerCreate { interval function context1 context2 -- timer ok }
 
 extern KeTimerInitialize { interval function context1 context2 timer -- ok }

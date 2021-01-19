ea56aaa0d544519757dcd74d213c1b5c8ea6d0fb
hyenasky <ashnwill49@gmail.com>
2021-01-19 02:50:13 -0700

    start of thread switching and stuff

--- OS/HAL/include/HALCPU.h
+++ OS/HAL/include/HALCPU.h
@@ -5,3 +5,7 @@ extern HALCPUInterruptDisable { -- rs }
 extern HALCPUInterruptRestore { rs -- }
 
 extern HALCPUInterruptEnable { -- }
+
+extern HALCPUContextInit { context1 context2 startfunc kstack -- ctx }
+
+extern HALCPUContextSwitch { old new -- }

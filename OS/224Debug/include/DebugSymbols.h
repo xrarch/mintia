30ccbe6d22ef5ccebf373873878a0c19f7ca0b2c
Will <ashnwill49@gmail.com>
2021-01-26 06:10:07 -0700

    add priority preemption and stuff, hopefully

--- OS/224Debug/include/DebugSymbols.h
+++ OS/224Debug/include/DebugSymbols.h
@@ -3,3 +3,5 @@ extern DebugGetSymbol { dll name -- symbol }
 extern DebugGetSymbolAddress { dll name -- address }
 
 extern DebugGetDLL { name -- dll }
+
+extern DebugGetSymDLL { pc -- dll symbase symname }
--- OS/HAL/limn2k/HALLimn2kHalt.s
+++ OS/HAL/limn2k/HALLimn2kHalt.s
@@ -1,4 +1,13 @@
 HALCPUHalt:
 .global HALCPUHalt
+	mov t0, sp
+
+	push lr
+	push t0
+
 	hlt
+
+	pop t0
+	pop lr
+
 	ret

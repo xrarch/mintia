37e8fd72d14074fe9cbf4c2cf5812a98070e514e
hyenasky <ashnwill49@gmail.com>
2021-05-12 04:52:49 -0600

    reorganize include files and start executive initialization

---
+++ OS/OSKernel/include/Memory/Mem.h
@@ -0,0 +1,52 @@
+struct MemBlock
+	4 Magic
+	4 Next
+	4 Prev
+	4 BucketIndex
+	4 Tag
+endstruct
+
+const MEMBLOCKMAGIC 0xC4FED33D
+
+const MEMBLOCKFREE 'FREE'
+
+struct PFDBE
+	4 R1
+	4 PFN
+	4 R3
+	4 R4
+	4 R5
+	4 R6
+	4 R7
+	4 R8
+endstruct
+
+struct PFDBEFree
+	4 Next
+	4 PFN
+	4 R2
+	4 R3
+	4 R4
+	4 R5
+	4 R6
+	4 R7
+endstruct
+
+extern MemUsageDump { -- }
+
+externptr MemPageFrameDB
+
+externptr MemPageFreeListHead
+
+externptr MemBuckets
+
+externptr MemPageFreeCount
+
+externptr MemHeapSize
+
+externptr MemHeapBytesFree
+
+externptr MemHeap
+
+const BUCKETSPO2START 5 // start at 2^5=32 bytes
+const BUCKETS 23 // 2^(23+5=28)=256MB heap max

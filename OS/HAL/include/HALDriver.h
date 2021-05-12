4c147caba87f051f654f59d7b081e50f8946369d
Will <ashnwill49@gmail.com>
2021-05-12 06:05:49 -0600

    add analog clock test driver

--- OS/HAL/include/HALDriver.h
+++ OS/HAL/include/HALDriver.h
@@ -1,3 +1,8 @@
 fnptr FDriverInitEarly { arg1 arg2 arg3 arg4 -- ok }
 
-fnptr FDriverInit { -- ok }
+fnptr FDriverInit { stage -- ok }
+
+extern HALDriverInit { stage ldrinfo -- }
+
+const STAGE_PRETASKING 1
+const STAGE_THREAD     2

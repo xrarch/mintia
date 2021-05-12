86278ad240f931923e92c5d756fc8d88e670d918
hyenasky <ashnwill49@gmail.com>
2021-05-12 06:05:49 -0600

    add analog clock test driver

--- OS/BootDrivers/LIMNstation/Dist.fst
+++ OS/BootDrivers/LIMNstation/Dist.fst
@@ -1,3 +1,4 @@
 # list of drivers to add to the distribution image
 
 AISA,clock:1.sys RTC/RTC.sys
+AnalogClockTest.sys AnalogClockTest/AnalogClockTest.sys
--- OS/HAL/limn2k/HALLimn2kFastDrawGlyph.s
+++ OS/HAL/limn2k/HALLimn2kFastDrawGlyph.s
@@ -4,27 +4,33 @@
 ;a1 - fb modulo
 ;a2 - start addr
 ;a3 - height
+;sp+8 - background
 HALCPUFastDrawGlyph:
 .global HALCPUFastDrawGlyph
-	li t4, 0x7FFF
+	mov v0, long [sp + 8]
 
 .yloop:
 	sub a3, a3, 1
 
-	li t1, 8
+	li  t1, 8
 
 	mov t2, byte [a0]
 
 .xloop:
 	sub t1, t1, 1
 
+	mov t4, v0
+
 	rsh t3, t2, t1
 	and t3, t3, 1
 	beq t3, zero, .nopix
 
-	mov int [a2], t4
+	li  t4, 0x7FFF
 
 .nopix:
+
+	mov int [a2], t4
+
 	add a2, a2, 2
 	bne t1, zero, .xloop
 

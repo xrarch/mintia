f9148e10e6933c2de3566a281a2f945fe46a70d3
seanbox <ashnwill49@gmail.com>
2021-08-29 05:40:58 -0600

    update readme, start filesystem api implementation

--- OS/OSKernel/include/IO/Device.h
+++ OS/OSKernel/include/IO/Device.h
@@ -7,6 +7,14 @@ struct IODriver
 	4 DispatchTable
 	4 ExtensionSize
 	4 BlockSize
+
+	4 Reserved1
+	4 Reserved2
+	4 Reserved3
+	4 Reserved4
+	4 Reserved5
+	4 Reserved6
+	4 Reserved7
 endstruct
 
 struct IODevice
@@ -14,4 +22,13 @@ struct IODevice
 	4 Extension
 	4 FileControlBlock
 	4 BlockSize
+	4 Mount
+
+	4 Reserved1
+	4 Reserved2
+	4 Reserved3
+	4 Reserved4
+	4 Reserved5
+	4 Reserved6
+	4 Reserved7
 endstruct

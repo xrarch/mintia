4c747411a75eea66fbe39d52c82b0315084b1e20
seanbox <ashnwill49@gmail.com>
2021-10-05 05:37:05 -0600

    Mm: much work. use new limn2600 pde semantics

--- OS/OSKernel/include/Memory/Pool.h
+++ OS/OSKernel/include/Memory/Pool.h
@@ -1,18 +1,5 @@
-struct MmPageFrameEntryPool
-	4 Next
-	4 PFN
-	4 Prev
-	4 BytesUsed
-	4 Context0
-	4 Context1
-	4 Context2
-	4 Context3
-	4 Context4
-	4 Context5
-	4 Context6
-	4 Context7
-	4 Context8
-	4 Context9
-	4 Context10
-	4 Context11
+struct MmPoolBlock
+	4 Tag
+	4 Magic
+	4 Size
 endstruct

bdebc89be4ecdbfbf211af8542004393aa9a4c61
hyenasky <ashnwill49@gmail.com>
2021-10-25 21:10:07 -0600

    Mm: begin work on anonymous memory and demand zero

--- OS/OSDLL/src/limn2600/DLLStart.s
+++ OS/OSDLL/src/limn2600/DLLStart.s
@@ -2,12 +2,19 @@
 
 __OSDLLStart:
 .global __OSDLLStart
-	b __OSDLLStart
+	la   t0, nonsense
+	mov  t0, long [t0]
+
+	la   t1, nonsense2
+	mov  t1, byte [t1]
+
+.loop:
+	b    .loop
 
 .section data
 
 nonsense:
-	.dl 0
+	.dl 0xABCDEF00
 
 .section bss
 
--- OS/OSKernel/include/Memory/Section.h
+++ OS/OSKernel/include/Memory/Section.h
@@ -8,8 +8,13 @@ struct MmSection
 	4 AnonSize
 	4 AnonPrototypePageTable
 
+	KeMutex_SIZEOF PrototypePageTableMutex
+
 	4 NoCommit
 endstruct
+
+extern MmSectionPPTLock { sectionobject -- ok }
+extern MmSectionPPTUnlock { sectionobject -- }
 
 extern MmSectionObjectCreateObject { pageprotection anonsize fileobject owninguser permissions name -- sectionobject ok }
 extern MmSectionObjectCreate { pageprotection anonsize filehandle owninguser permissions name -- sectionhandle ok }

621dc98feff25ac1a71bd7c32cafd02d8bebd330
hyenasky <ashnwill49@gmail.com>
2021-10-30 11:04:12 -0600

    IO: make page cache write/read data to/from userspace safely

--- OS/OSKernel/include/FSRTL/FSVolume.h
+++ OS/OSKernel/include/FSRTL/FSVolume.h
@@ -5,8 +5,5 @@ extern FSVolumeFlush { trunc mount -- ok }
 extern FSVolumeRead { length offset buf lastmode mount -- bytesread ok }
 extern FSVolumeWrite { length offset buf lastmode mount -- byteswritten ok }
 
-extern FSVolumeReadCached { length offset buf mount -- bytesread ok }
-extern FSVolumeWriteCached { length offset buf mount -- byteswritten ok }
-
 extern FSVolumeBlockRead { flags kflags offset mount -- pageframe pfdbe ok }
 extern FSVolumeBlockWrite { pfdbe mount -- ok }

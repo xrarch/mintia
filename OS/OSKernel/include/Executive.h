7f4167b3b81bd81ef85a32ca8a65f7039b94090b
seanbox <ashnwill49@gmail.com>
2021-11-02 18:59:01 -0600

    Ex: replace handle table mutex with RwLock

--- OS/OSKernel/include/Executive.h
+++ OS/OSKernel/include/Executive.h
@@ -1,8 +1,8 @@
+#include "<inc>/Executive/RwLock.h"
 #include "<inc>/Executive/Handle.h"
 #include "<inc>/Executive/RingBuffer.h"
 #include "<inc>/Executive/Log.h"
 #include "<inc>/Executive/Bitmap.h"
-#include "<inc>/Executive/RwLock.h"
 #include "<inc>/Executive/Call.h"
 
 extern ExInitPhase0 { -- }

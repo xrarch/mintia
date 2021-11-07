b71a3545a0eb878f30eaf236642db4e1ad9b9cc2
hyenasky <ashnwill49@gmail.com>
2021-11-07 00:40:47 -0600

    OSDLL: hopefully implement userspace heap

--- OS/OSDLL/headers/OS.h
+++ OS/OSDLL/headers/OS.h
@@ -5,6 +5,5 @@
 #include "<ll>/OSDLL/OSMap.h"
 #include "<ll>/OSDLL/OSSignal.h"
 #include "<ll>/OSDLL/OSCalls.h"
-#include "<ll>/OSDLL/OSPEB.h"
 #include "<ll>/OSDLL/OSAbort.h"
 #include "<ll>/OSDLL/OSObject.h"
--- OS/OSKernel/include/Kernel/Dispatch.h
+++ OS/OSKernel/include/Kernel/Dispatch.h
@@ -19,9 +19,10 @@ struct KeDispatchHeader
 	4 Reserved2
 endstruct
 
-// a thread can wait on up to THREADWAITBLOCKS dispatcher objects at a time, and a
-// dispatcher object can be waited on by an arbitrary number of threads. WaitBlocks
-// are the data structure that accomplishes this feature of the kernel.
+// a thread can wait on up to THREADWAITBLOCKS dispatcher objects at a time,
+// and a dispatcher object can be waited on by an arbitrary number of threads.
+// WaitBlocks are the data structure that accomplishes this feature of the
+// kernel.
 
 struct KeDispatchWaitBlock
 	4 Prev

0eb0208772c98add629df18892eefb0354f7e731
hyenasky <ashnwill49@gmail.com>
2021-12-11 16:22:05 -0700

    Ob: clarify some comments; Keyboard: start driver

--- OS/BootDrivers/LIMNstation/Dist.fst
+++ OS/BootDrivers/LIMNstation/Dist.fst
@@ -5,5 +5,6 @@ AISA,serial:1.sys Serial/Serial.sys 493
 AISA,satsuma:1.sys DKS/DKS.sys 493
 AISA,ebus:1.sys EBus/EBus.sys 493
 AISA,kinnowfb:1.sys KinnowFB/KinnowFB.sys 493
+AISA,keyboard:2.sys Keyboard/Keyboard.sys 493
 
 AnalogClockTest.sys AnalogClockTest/AnalogClockTest.sys 493
---
+++ OS/HAL/LIMNstation/include/HALLIMNstationAmtsu.h
@@ -0,0 +1,26 @@
+fnptr HALLIMNstationAmtsuCallbackF { id -- }
+
+extern HALLIMNstationAmtsuSelect { num -- }
+
+extern HALLIMNstationAmtsuCheckMID { num -- mid }
+extern HALLIMNstationAmtsuReadMID { -- mid }
+
+extern HALLIMNstationAmtsuCommand { cmd -- }
+extern HALLIMNstationAmtsuCommandAsync { cmd -- }
+
+extern HALLIMNstationAmtsuWriteA { long -- }
+extern HALLIMNstationAmtsuWriteB { long -- }
+
+extern HALLIMNstationAmtsuReadA { -- long }
+extern HALLIMNstationAmtsuReadB { -- long }
+
+extern HALLIMNstationAmtsuSetInterrupt { dev -- }
+extern HALLIMNstationAmtsuClearInterrupt { dev -- }
+
+extern HALLIMNstationAmtsuSpecialCMD { a b cmd -- }
+
+extern HALLIMNstationAmtsuReset { -- }
+
+extern HALLIMNstationAmtsuEnumerate { func mid -- count }
+
+extern HALLIMNstationAmtsuIrqToID { irq -- id }

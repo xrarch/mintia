c51fcbb78777fcefd679986bee7a1ba3e80ea888
hyenasky <ashnwill49@gmail.com>
2021-12-13 23:34:20 -0700

    Ps: make PIDs a thing

--- OS/OSKernel/include/Executive/Handle.h
+++ OS/OSKernel/include/Executive/Handle.h
@@ -17,10 +17,13 @@ endstruct
 
 extern ExHandleTableDelete { func handletable -- }
 extern ExHandleTableCreate { chargedprocess entrysizelog -- handletable ok }
+extern ExHandleTableDuplicate { func handletablesrc chargedprocess -- handletable ok }
+
+extern ExHandleTableLockUnalertable { handletable -- }
+extern ExHandleTableLockSharedUnalertable { handletable -- }
 extern ExHandleTableLock { handletable -- ok }
 extern ExHandleTableLockShared { handletable -- ok }
 extern ExHandleTableUnlock { handletable -- }
-extern ExHandleTableDuplicate { func handletablesrc chargedprocess -- handletable ok }
 
 extern ExHandleDelete { handle handletable locked -- entryptr entryvalue ok }
 extern ExHandleCreate { entryvalue handletable locked -- handle entryptr ok }

57467818561476de3e716fedffa64868d31c46e4
hyenasky <ashnwill49@gmail.com>
2021-12-15 06:07:27 -0700

    HAL, Ke: move exception handler into kernel where it belongs

--- OS/HAL/limn2600/HALLimn2500Init.s
+++ OS/HAL/limn2600/HALLimn2500Init.s
@@ -3,6 +3,7 @@
 .section text
 
 HALLimn2500SavedEV:
+.global HALLimn2500SavedEV
 	.dl 0
 
 HALCPUExit:
@@ -22,20 +23,4 @@ HALLimn2500Exit:
 	mov  lr, long [sp]
 	addi sp, sp, 4
 
-	ret
-
-HALCPUInit:
-.global HALCPUInit
-	la   t0, HALLimn2500SavedEV
-	mfcr t1, evec
-	mov  long [t0], t1
-
-	la   t0, HALLimn2500ExceptionVector
-	mtcr evec, t0
-
-	mtcr ebadaddr, zero
-
-	subi t0, zero, 1
-	ftlb t0, t0
-	
 	ret
--- OS/OSKernel/include/Kernel/IPL.h
+++ OS/OSKernel/include/Kernel/IPL.h
@@ -4,5 +4,7 @@ extern KeIPLRaise { newipl -- oldipl }
 extern KeIPLLower { newipl -- }
 extern KeIPLCurrentGet { -- ipl }
 
+extern KeInterrupt { ipl int handler -- }
+
 const KERNELMODE 1
 const USERMODE 2

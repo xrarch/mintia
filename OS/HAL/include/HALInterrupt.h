bfb429453b5523ac7989e7d1c0f85362ee8028d7
hyenasky <ashnwill49@gmail.com>
2021-12-15 17:20:44 -0700

    HAL, Ke: move LSIC stuff back to HAL

--- OS/HAL/include/HALInterrupt.h
+++ OS/HAL/include/HALInterrupt.h
@@ -1,5 +1,8 @@
 extern HALInterruptRegister { handler int ipl -- }
 extern HALInterruptUnregister { int -- }
 
+extern HALPlatformInterrupt { keint -- }
+fnptr HALKeInterruptF { ipl int handler -- }
+
 fnptr HALInterruptHandler { int -- }
 fnptr HALPageFaultHandler { writing badaddr trapframe -- handled }

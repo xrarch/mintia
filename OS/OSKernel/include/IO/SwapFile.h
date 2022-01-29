283c5779256e2bb1b5bb749a2205d0d64827c918
hyenasky <ashnwill49@gmail.com>
2022-01-28 18:29:57 -0700

    Mm: don't start modified page writing until swapfile created

--- OS/OSKernel/include/IO/SwapFile.h
+++ OS/OSKernel/include/IO/SwapFile.h
@@ -3,6 +3,7 @@ extern IOSwapFileCreate { filehandle -- sizeinpages ok }
 extern IOSwapPageFree { pno -- }
 extern IOSwapPageWrite { pageframe -- pno ok }
 extern IOSwapPageRead { pageframe pno -- ok }
+extern IOSwapFileAwaitCreation { -- }
 
 externptr IOSwapPagesUsed
 externptr IOSwapPagesCount

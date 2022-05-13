5d826e222b250563a7af70670c927dfa9f360f41
hyenasky <ashnwill49@gmail.com>
2022-05-13 05:13:09 -0600

    AisixFS: cluster contiguous IO

--- OS/OSKernel/include/FSRTL/FSVolume.h
+++ OS/OSKernel/include/FSRTL/FSVolume.h
@@ -7,3 +7,8 @@ extern FSVolumeWrite { length offset buf lastmode mount -- byteswritten ok }
 
 extern FSVolumeBlockRead { flags kflags offset mount -- pageframe pfdbe ok }
 extern FSVolumeBlockWrite { pfdbe mount -- ok }
+
+extern FSVolumeGetFCB { fcb -- volfcb }
+
+extern FSVolumeFCBGetReadFunction { volfcb -- readfunc }
+extern FSVolumeFCBGetWriteFunction { volfcb -- writefunc }

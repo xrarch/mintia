c0965a293dc65eab8d798e79a5239bd7baed2f5c
hyenasky <ashnwill49@gmail.com>
2022-09-24 05:17:58 -0600

    IPC: leetle more work

--- OS/OSKernel/include/IPC/Port.h
+++ OS/OSKernel/include/IPC/Port.h
@@ -2,17 +2,31 @@ struct IPCPort
 	4 MessageListHead
 	4 MessageListTail
 
+	4 ReceiverListHead
+	4 ReceiverListTail
+
 	4 OwningProcess
 	4 ClientTableHeader
 	4 ServerPort
 
 	4 ClientID
+
+	4 ServerContextForClient
+
+	4 Flags
 endstruct
+
+const IPCPORTFLAG_CLIENT            1
+const IPCPORTFLAG_FAILED_CONNECTION 2
 
 extern IPCInitPhase1 { -- }
 
 extern IPCPortObjectDelete { object -- }
+extern IPCPortObjectOpen { access object process -- ok }
 
 extern IPCPortCreateObject { serverport owningprocess permissions name -- portobject ok }
 
 externptr IPCPortObjectType
+
+extern IPCGlobalLock { -- ok }
+extern IPCGlobalUnlock { -- }

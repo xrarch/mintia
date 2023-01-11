8d5946333eb11ad56a220667f8bee8ba5abc8fd8
hyenasky <ashnwill49@gmail.com>
2023-01-11 03:52:39 -0700

    Mcl: begin sketching an outline

--- OS/MCL/Mcl.h
+++ OS/MCL/Mcl.h
@@ -1,3 +1,54 @@
-struct MclMachine
-	OSFastMutex_SIZEOF Mutex
+const SYMBOLHASHTABLESIZE 97
+
+struct MclpSymbolTable
+	4 Count
+	4 ListHead
+
+	(SYMBOLHASHTABLESIZE 4 *) BucketHeads
 endstruct
+
+struct MclpMachine
+	OSFastMutex_SIZEOF Mutex
+
+	MclpSymbolTable_SIZEOF SymbolTable
+endstruct
+
+struct MclpValue
+	4 Type
+	4 RefCount
+
+	4 Context
+endstruct
+
+struct MclpSymbol
+	4 SymbolTable
+
+	4 BucketNext
+	4 BucketPrev
+	4 BucketIndex
+
+	4 GlobalNext
+	4 GlobalPrev
+
+	4 Value
+
+	0 Name
+endstruct
+
+struct MclpTypeOperations
+	4 Create
+	4 Free
+endstruct
+
+struct MclpType
+	4 Name
+
+	MclpTypeOperations_SIZEOF Ops
+endstruct
+
+extern MclpMachineLock { machine -- }
+extern MclpMachineUnlock { machine -- }
+
+extern MclpParseFile { filehandle machine -- ok }
+
+extern MclpSymbolTableInitialize { symboltable -- }

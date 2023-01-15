84278847bcfbb22b554767dead7e8554ec7f1fc6
hyenasky <ashnwill49@gmail.com>
2023-01-15 15:14:25 -0700

    Ex: generalize referencing an object in a handle table

--- OS/OSKernel/include/Executive/Handle.h
+++ OS/OSKernel/include/Executive/Handle.h
@@ -29,3 +29,5 @@ extern ExHandleTableUnlock { handletable -- }
 extern ExHandleDelete { handle handletable locked -- entryptr entryvalue ok }
 extern ExHandleCreate { entryvalue handletable locked -- handle entryptr ok }
 extern ExHandleLookup { handle handletable locked -- entryptr ok }
+
+extern ExHandleTableLookupAndReferenceObject { handle handletable locked -- object ok }

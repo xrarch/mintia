b74901ca618a697cef6ff81426bc35c39e63fcf7
hyenasky <ashnwill49@gmail.com>
2023-02-18 15:54:40 -0700

    XRstation: combine chipset drivers under the ebus driver umbrella
    saves ~36K of wired memory

--- XRstation.mk
+++ XRstation.mk
@@ -1,9 +1,6 @@
 ARCHITECTURE := xr17032
 
-KERNELMODULES += BootDrivers/XRstation/DKS \
-		BootDrivers/XRstation/EBus \
+KERNELMODULES += BootDrivers/XRstation/EBus \
 		BootDrivers/XRstation/Keyboard \
 		BootDrivers/XRstation/KinnowFB \
-		BootDrivers/XRstation/Mouse \
-		BootDrivers/XRstation/RTC \
-		BootDrivers/XRstation/Serial
+		BootDrivers/XRstation/Mouse

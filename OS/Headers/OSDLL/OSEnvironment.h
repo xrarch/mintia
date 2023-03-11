15a35f402dfb806ebf7335bf5901293f3e656a11
hyenasky <ashnwill49@gmail.com>
2023-03-11 07:54:03 -0700

    Commands: add env command

--- OS/Headers/OSDLL/OSEnvironment.h
+++ OS/Headers/OSDLL/OSEnvironment.h
@@ -1,4 +1,7 @@
+fnptr OSEnumerateEnvironmentF { entry -- ok }
+
 extern OSFindEnvironmentVariable { name -- entry env ok }
 extern OSGetEnvironmentVariable { env name bufsize -- ok }
 extern OSSetEnvironmentVariable { env name -- ok }
 extern OSDeleteEnvironmentVariable { name -- ok }
+extern OSEnumerateEnvironment { func -- ok }

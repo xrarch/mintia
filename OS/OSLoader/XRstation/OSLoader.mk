a1681d01d3021e48a6b8db45e4ce626b07a0b888
hyenasky <ashnwill49@gmail.com>
2023-03-10 19:05:32 -0700

    OSLoader: fix linking on XRstation

--- OS/OSLoader/XRstation/OSLoader.mk
+++ OS/OSLoader/XRstation/OSLoader.mk
@@ -1,6 +1,6 @@
 OUTPUTFILE := OSLoader.a3x
 
-PRELIBS  := LX/limn2600/rta3x/_boot.o
+PRELIBS  := LX/xr17032/rta3x/_boot.o
 LIBS     := L/rta3x/rta3x.o
 MOVEEXPR := text=0x20400,data=text+text_size,bss=data+data_size
 MKBINARY := yes

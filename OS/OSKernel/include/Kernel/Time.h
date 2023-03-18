3ab4e63da3050c453c34977ee3a00e34c07f4071
hyenasky <ashnwill49@gmail.com>
2023-03-17 20:47:08 -0600

    Ke: remove superfluous time functions

--- OS/OSKernel/include/Kernel/Time.h
+++ OS/OSKernel/include/Kernel/Time.h
@@ -3,12 +3,3 @@ struct KeTime
 	4 SecPart
 	4 MsPart
 endstruct
-
-extern KeTimeAddMs { ms time -- }
-extern KeTimeSubMs { ms time -- }
-
-// timedest = time1 + time2
-extern KeTimeAdd { time1 time2 timedest -- }
-
-// timedest = time1 - time2
-extern KeTimeSub { time1 time2 timedest -- }

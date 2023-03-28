d7b9e854eb9a648a7f327c67b8c3227e4b6e44d8
hyenasky <ashnwill49@gmail.com>
2023-03-28 01:16:24 -0600

    Fat: create volume label dirent if its not found

--- OS/BootDrivers/Fat/Fat.h
+++ OS/BootDrivers/Fat/Fat.h
@@ -339,6 +339,7 @@ extern FatInitializeGenerationContext { context -- }
 extern FatGenerateShortName { shortname longname context -- ok }
 extern FatConvertToShortName { shortname name -- }
 extern FatBuildDotEntry { name fatdate fattime fcb fatdirent -- }
+extern FatCopyVolumeLabel { fatdirent dest src -- }
 
 struct FatShortNameContext
 	4 Index

5a0eb77bd6950fe6ba89ffc4ec18e23f451e0c49
hyenasky <ashnwill49@gmail.com>
2023-06-01 06:29:10 -0600

    Ke: more spring cleaning

--- OS/HAL/include/HALCPU.h
+++ OS/HAL/include/HALCPU.h
@@ -6,8 +6,6 @@ extern HALCPUInterruptEnable { -- }
 
 extern HALCPUContextInit { context1 context2 startfunc kstack process -- ctx }
 extern HALCPUContextSwitch { old new -- }
-extern HALCPURunInContext { int handler stack -- }
-extern HALCPURunInContextNoInterrupts { handler stack arg1 arg0 -- ret0 ret1 }
 extern HALCPUIsUserTrapFrame { tf -- user }
 extern HALCPUTrapFrameAbort { ok abort tf -- }
 

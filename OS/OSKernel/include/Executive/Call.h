628eeae4db7dc3294b41c47533fdce8f6293b2cb
hyenasky <ashnwill49@gmail.com>
2023-07-07 07:46:13 -0600

    Ex: use a pinned user buffer for some syscalls
    rather than a temporary nonpaged pool buffer

--- OS/OSKernel/include/Executive/Call.h
+++ OS/OSKernel/include/Executive/Call.h
@@ -1,2 +1,5 @@
 extern ExCallGetString { max string -- buf ok }
 extern ExCallFreeString { buf -- }
+
+extern ExCallGetUserBuffer { lockforwrite length vaddr -- buf mdl ok }
+extern ExCallFreeUserBuffer { mdl -- }
